d9e5bba326c891709fe2ec46a06e87fe7e0a4862
jesse gempel <gempel@opsys.cs.umsl.edu>
2025-03-13 16:07:47 -0500

    worker.c: added memory segment for receiving filename from oss.c
    worker.c: added code for initializing and removing a message queue

--- worker.c
+++ worker.c
@@ -5,13 +5,14 @@
 */
 
 
-// The user.c file works with CHILD processes.
+// The worker.c file works with CHILD processes.
 // It prints out child and parent process IDs, as well as child process and termination times, to the user for a specific period of time.
 
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/msg.h>
 #include <sys/shm.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,11 +23,36 @@
 #define SHMKEY1 42069
 #define INT_BUFFER_SIZE sizeof(int)
 
+
 // Starting a memory segment for system clock nanoseconds.
 #define SHMKEY2 42070
 #define LONG_BUFFER_SIZE sizeof(long int)
 
 
+// Starting a memory segment for a log file to validate ftok() function.
+#define SHMKEY3 42071
+#define LOGFILE_BUFFER_SIZE 105
+
+
+// Permissions for memory queue.
+#define PERMISSIONS 0644
+
+
+// Holds message queue information.
+typedef struct messageBuffer {
+   long int messageType;
+   char stringData[100];
+   int integerData;
+} messageBuffer;
+
+
+// Initializes information for message buffer.
+messageBuffer buffer;
+int messageQueueID;
+key_t key;
+
+
+// Gathers logfile name from oss.c to validate ftok() function in worker.c (this file).
 int childProcessTimeSeconds;
 int childTerminationTimeSeconds;
 long int childProcessTimeNano;
@@ -58,13 +84,21 @@ int getElapsedTimeSeconds (int startingTimeSeconds, int currentTimeSeconds) {
 
 int main(int argc, char** argv) {
   
-   // Creates two shared memory identifiers.
+   // Creates two shared memory identifiers (and one for a log file).
    int secondsShmid = shmget(SHMKEY1, INT_BUFFER_SIZE, 0777);
    long int nanoShmid = shmget(SHMKEY2, LONG_BUFFER_SIZE, 0777);
+
+   int logfileShmid = shmget(SHMKEY3, LOGFILE_BUFFER_SIZE, 0777);
+
 
    // Attaches the system time into shared memory.
    int *sharedSeconds = (int *)shmat(secondsShmid, 0, 0);
    long int *sharedNanoseconds = (long int *)shmat(nanoShmid, 0, 0);
+
+
+   // Attaches log file into shared memory.
+   char *logfileFP = (char *)shmat(logfileShmid, 0, 0);
+
 
    // Used for constant system time updated from shared memory.
    int systemClockSeconds = *sharedSeconds;
@@ -73,12 +107,34 @@ int main(int argc, char** argv) {
    // Used for termination time calculations. Will not be updated.
    int initialSystemClockSecs = *sharedSeconds;
    long int initialSystemClockNano = *sharedNanoseconds;        
-  
+ 
+   printf("logfile in worker.c: %s", logfileFP);
+
+
+
+
+   // Attempts to set up a message queue.
+   if ((key = ftok(logfileFP, 1)) == -1) {
+      printf("ERROR in worker.c: problem with ftok() function.\n");
+      printf("Cannot access a key for message queue initialization.\n\n");
+
+      exit(-1);
+   }
+
+   if ((messageQueueID = msgget(key, PERMISSIONS)) == -1) {
+      printf("ERROR in worker.c: problem with msgget() function.\n");
+      printf("Cannot acquire a message queue ID for initialization.\n\n");
+
+      exit(-1);
+   }
+
+   printf("Message queue is now set up in worker.c.\n\n");
+
 
    // If executing ./worker [timeLimitForChildren] [timeLimitNanoseconds], user must enter two integers after ./worker.
    // Otherwise, present error and terminate program.
    if (argc != 3) {
-      printf("ERROR in 'worker.c': You must enter ./user followed by two integers.\n\n");
+      printf("ERROR in 'worker.c': You must enter ./worker followed by two integers.\n\n");
 
       printf("Integer 1: the maximum time limit for a child process (in seconds).\n");
       printf("Integer 2: the # of nanoseconds AFTER seconds time limit has been reached.\n\n");
@@ -116,6 +172,7 @@ int main(int argc, char** argv) {
 
          int secondsRan = getElapsedTimeSeconds(initialSystemClockSecs, systemClockSeconds);
  
+//         printf("systemClockSeconds: %d\t systemClockNano: %ld\nFrom worker.c\n", systemClockSeconds, systemClockNano);
 
          // Ensures that # of nanoseconds is similar to the last second as to the current second.
 	 // That way, the statement below will wait one COMPLETE second before printing again.
@@ -157,7 +214,7 @@ int main(int argc, char** argv) {
    // Detach shared memory.
    shmdt(sharedSeconds);
    shmdt(sharedNanoseconds);
-
+   shmdt(logfileFP);
 
    return EXIT_SUCCESS;
 
